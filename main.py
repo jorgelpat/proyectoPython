7f5382b60a699ff94452dab4b50185d139874165
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-01 23:30:53 -0500

    feat:  :construction: se crea menu()

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #Creamos nuestro primera carpeta
+
+def menu():
+    print("Seguimiento Academico de Campers")
+    print("\t1. Registro de camper")
+    print("\t2. Registro de pruebas")
+    print("\t3. Registro de areas de entrenamiento")
+    print("\t4. Crear rutas de entrenamiento")
+
