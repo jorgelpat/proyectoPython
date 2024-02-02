950ee579448475de0b3a88ce38b8700843689636
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-02 00:07:31 -0500

    feat:  :construction: saltos de linea

--- modulo/camper.py
+++ modulo/camper.py
@@ -3,11 +3,11 @@ from os import system
 def guardar():
     system("clear")
     info = {
-        "Nombre": input("Ingrese nombre(s) del camper"),
-        "Apellido": input("Ingrese apelido(s) del camper"),
-        "Identificacion": input("Ingrese numero de identificacion"),
-        "Direccion": input("Indique direccion de residencia"),
-        "Acudiente": input("Indique nombre completo del acudiente"),
-        "Telefono": int("Indique numero de contacto"),
-        "Estado": input("Estado del camper")
+        "Nombre": input("Ingrese nombre(s) del camper\n"),
+        "Apellido": input("Ingrese apelido(s) del camper\n"),
+        "Identificacion": input("Ingrese numero de identificacion\n"),
+        "Direccion": input("Indique direccion de residencia\n"),
+        "Acudiente": input("Indique nombre completo del acudiente\n"),
+        "Telefono": int("Indique numero de contacto\n"),
+        "Estado": input("Indique el estado del camper\n")
     }
