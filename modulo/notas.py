d030997ff8726ba9406a722d5dcbd6b5fee174f1
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-02 23:54:33 -0500

    feat:  :construction:  guardar()
    Se agrega codigo a la funcion guardar

---
+++ modulo/notas.py
@@ -0,0 +1,33 @@
+from os import system
+
+def guardar():
+    infoNota = {
+        "Teorica":"",
+        "Practica":"",
+        "Trabajos":""
+    }
+    bandera=True
+    while (bandera):
+        teorica = input("Prueba teorica: ")
+        if teorica.isnumeric():
+            infoNota["Teorica"]=teorica
+            bandera = False
+        else:
+            print("Dato no Valido")
+    bandera=True
+    while (bandera):
+        practica = input("Prueba practica: ")
+        if practica.isnumeric():
+            infoNota["Practica"]=practica
+            bandera = False
+        else:
+            print("Dato no Valido")
+    bandera=True
+    while (bandera):
+        trabajos = input("Quizes y trabajos: ")
+        if trabajos.isnumeric():
+            infoNota["Trabajos"]=trabajos
+            bandera = False
+        else:
+            print("Dato no Valido")
+
