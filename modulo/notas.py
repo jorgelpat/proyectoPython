f19556235860687ce330ea002e807cfd7369410c
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-04 17:04:23 -0500

    feat:  :construction:  import json

--- modulo/notas.py
+++ modulo/notas.py
@@ -1,4 +1,8 @@
 from os import system
+import json
+from .data import estados, camper
+
+
 
 def guardar():
     system("clear")
@@ -32,3 +36,8 @@ def guardar():
         else:
             print("Dato no Valido")
 
+def buscar():
+    user_imput = int(input("Indique la identificacion del camper"))
+    for camper in list(camper):
+        if camper[id]==user_imput:
+            print("".format(user_imput))
