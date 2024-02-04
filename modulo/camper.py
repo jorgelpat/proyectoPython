ecf85f5751a713d1ed17fc81168702be604b2f06
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-04 00:10:55 -0500

    feat:  :construction:  se agrega crud
    Se agregan las funciones crud y se corrige sobreescritura de los datos en camper.json

--- modulo/camper.py
+++ modulo/camper.py
@@ -46,14 +46,14 @@ def guardar():
             bandera = False
         else:
              print("Dato no Valido")
-    infoCamp["Estado"] = input("Elija estado del camper:\n\t"+"\t".join([f"{estados.index(i)+1}. {i}\n"for i in estados]))
-    
+    infoCamp["Estado"] = input("Elija estado del camper:\n\t"+"\t".join([f"{estados.index(i)+1}. {i}\n" for i in estados]))
+    with open("modulo/storage/camper.json") as f:
+        camper = json.load(f)
     camper.append(infoCamp)
     with open("modulo/storage/camper.json", "w") as f:
         data = json.dumps(camper, indent=4)
         f.write(data)
         f.close()
-    menu()
     return "Guardado Exitoso"
 
 
@@ -81,16 +81,58 @@ Telefono: {camper[code].get('Telefono')}
             opc = int(input())
             if opc == 1:
                 system("clear")
-                camper.pop(code)
-                with open("modulo/storage/camper.json", "w") as f:
+                infoCamp = {
+                "Nombre": "",
+                "Apellido": "",
+                "Edad": "",
+                "Id": "",
+                "Direccion": "",
+                "Acudiente": "",
+                "Telefono": "",
+                "Estado": ""
+                }
+                infoCamp["Nombre"] = input("Ingrese nombre(s) del camper\n")
+                infoCamp["Apellido"] = input("Ingrese apellido(s) del camper\n")
+                bandera = True
+                while (bandera):
+                    edad = input("Ingrese edad\n")
+                    if edad.isnumeric():
+                        infoCamp["Edad"]=edad
+                        bandera = False
+                    else:
+                        print("Dato no Valido")
+                bandera = True
+                while (bandera):
+                    id = input("Ingrese numero de identificacion\n")
+                    if id.isnumeric():
+                        infoCamp["Id"]=id
+                        bandera = False
+                    else:
+                        print("Dato no Valido")
+                infoCamp["Direccion"] = input("Indique direccion de residencia\n")
+                if int(infoCamp["Edad"])<18:
+                    infoCamp["Acudiente"] = input("Indique nombre completo del acudiente\n")
+                else:
+                    infoCamp["Acudiente"] = None
+                bandera = True
+                while (bandera):
+                    tel = input("Indique numero de contacto\n")
+                    if tel.isnumeric():
+                        infoCamp["Telefono"]=tel
+                        bandera = False
+                    else:
+                        print("Dato no Valido")
+                infoCamp["Estado"] = input("Elija estado del camper:\n\t"+"\t".join([f"{estados.index(i)+1}. {i}\n"for i in estados]))
+                camper[code]=infoCamp
+                with open("modulo/storage/camper.json","w") as f:
                     data = json.dumps(camper, indent=4)
                     f.write(data)
                     f.close()
-                bandera=False
             if opc == 2:
                 actualizar()
             if opc == 3:
                 bandera = False
+        return "Edicion Terminada"
 
 
 def buscar():
@@ -112,7 +154,36 @@ Estado: {val.get('Estado')}
 
 
 def eliminar():
-    print()
+    bandera=True
+    while (bandera):
+        system("clear")
+        code = int(input("Ingrese codigo del camper\n"))
+        print(f"""
+codigo: {code}
+Nombre: {camper[code].get('Nombre')}              
+Apellido: {camper[code].get('Apellido')}
+Edad: {camper[code].get('Edad')}
+Id: {camper[code].get('Id')}
+Direccion: {camper[code].get('Direccion')}
+Acudiente: {camper[code].get('Acudiente')}
+Telefono: {camper[code].get('Telefono')}              
+              """)
+        print("¿Este es el camper que deseas eliminar?")
+        print("1. Si")
+        print("2. No")
+        print("3. Salir")
+        opc = int(input())
+        if opc==1:
+            camper.pop(code)
+            with open("modulo/storage/camper.json", "w") as f:
+                data = json.dumps(camper,indent=4)
+                f.write(code)
+                f.close()
+            bandera = False
+        if opc==2:
+            eliminar()
+        if opc==3:
+            bandera = False
 
 def menu():
     system("clear")
