d70b905b537d83073dd0940bea6f5b2432fd8fd2
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-09 19:03:33 -0500

    feat:  :construction:  funciones agregar trainer,camper,hora
    se crean las funciones para agregar trainers y campers a los horarios

---
+++ modulo/horarios.py
@@ -0,0 +1,89 @@
+def ingreso_camper_hora1():
+    id = input("Indique id del camper que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    if len(infArea1["Campers"])<33:
+        infArea1["Campers"].append(id)
+    else:
+        print("Ya hay un maximo de 33 campers")
+    return "Campers Agregados"
+
+def ingreso_trainer_hora1():
+    id = input("Indique id del trainer que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    infArea1["Trainer"]=id
+
+def ingreso_camper_hora2():
+    id = input("Indique id del camper que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    if len(infArea1["Campers"])<33:
+        infArea1["Campers"].append(id)
+    else:
+        print("Ya hay un maximo de 33 campers")
+    return "Campers Agregados"
+
+def ingreso_trainer_hora2():
+    id = input("Indique id del trainer que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    infArea1["Trainer"]=id
+
+def ingreso_camper_hora3():
+    id = input("Indique id del camper que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    if len(infArea1["Campers"])<33:
+        infArea1["Campers"].append(id)
+    else:
+        print("Ya hay un maximo de 33 campers")
+    return "Campers Agregados"
+
+def ingreso_trainer_hora3():
+    id = input("Indique id del trainer que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    infArea1["Trainer"]=id
+
+def ingreso_camper_hora4():
+    id = input("Indique id del camper que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    if len(infArea1["Campers"])<33:
+        infArea1["Campers"].append(id)
+    else:
+        print("Ya hay un maximo de 33 campers")
+    return "Campers Agregados"
+
+def ingreso_trainer_hora4():
+    id = input("Indique id del trainer que desea agregar:\n")
+    infArea1={
+        "Campers": [],
+        "Trainer": "",
+        "Area": "1"
+    }
+    infArea1["Trainer"]=id
+
+#Hace falta editar los datos para que se abran con el json
