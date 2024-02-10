d8fac7571f2c44fa83cdcc925dc78e7bd66f6401
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-10 18:45:31 -0500

    feat:  :construction:  se borran datos y se corrige codigo

--- modulo/notas.py
+++ modulo/notas.py
@@ -1,72 +1,6 @@
 from os import system
 import json
-
 from .data import notas, camper
-
-
-
-
-
-
-def preNotas():
-    notas={
-        "Id": "",
-        "Teorica": "",
-        "Practica": ""
-    }
-    bandera=True
-    while(bandera):
-        id=input("Identificacion del camper:\n")
-        if id.isnumeric():
-            notas["Id"]=id
-            bandera=False
-        else:
-            print("Dato no valido")
-    bandera=True
-    while(bandera):
-        teorica=input("Nota teorica: ")
-        if teorica.isnumeric():
-            notas["Teorica"]= teorica 
-            bandera=False
-    bandera=True
-    while(bandera):
-        practica=input("Nota practica: ")
-        if practica.isnumeric():
-            notas["Practica"]=practica 
-            bandera=False
-
-    with open("modulo/storage/preNotas.json") as f:
-        campers=json.loads(f.read())
-        campers.append(notas)
-    with open("modulo/storage/preNotas.json","w") as f:
-        data=json.dumps(campers, indent=4)
-        f.write(data)
-        f.close()
-    # for datos in data:
-        # numer1=int(datos["Teorica"])
-        # numer2=int(datos["Practica"])
-        # if (datos["Teorica"]+datos["Practica"])/2 > 60:#Corregir aqui
-            # with open("modulo/storage/camper.json") as f:
-            #     campers1=json.loads(f.read())
-            #     f.close()
-            #     for camper1 in campers1:
-            #         if id==camper1["Id"]:
-            #             with open("modulo/storage/camper.json","w") as f:
-            #                 data1=json.dumps(f.write(data1))
-            #                 f.close()
-            #                 data1["Estado"]="Inscrito"
-
-
-    return "Datos cargados" #Procurar hacer una funcion que registre notas y si es preinscrito solo pedirle 2
-#si es incsrito, pedirle 3 y guardarlas y si es aprobado guardarlas en otro Json
-
-
-def contar_modulo(jsondata,clave,valor):
-    contador=0
-    for k, v in jsondata.items():
-        if k==clave:
-            contador+=v.count(valor)  #Contador no terminado
-
 
 
 
@@ -133,7 +67,7 @@ def guardarNotasModulo():
                         if practica.isnumeric():
                             infoNota["Practica"]=practica
                             bandera1=False
-                    if int(practica)+int(teorica)/2 >= 60:
+                    if (int(practica)+int(teorica))/2 >= 60:
                         camper["Estado"]="Aprobado"
                     else:
                         camper["Estado"]="Filtrado"
@@ -155,55 +89,6 @@ def guardarNotasModulo():
     return "Guardado"
 
 
-
-    # #system("clear")
-    # infoNota = {
-    #     "Id":"",
-    #     "Teorica":"",
-    #     "Practica":"",
-    #     "Trabajos":""
-    # }
-    # bandera=True
-    # while (bandera):
-    #     id = input("Escriba el documento del camper:")
-    #     if id.isnumeric():
-    #         infoNota["Id"] = id
-    #         bandera=False
-    #     else:
-    #         print("Dato no Valido")
-    #     teorica = input("Prueba teorica: ")
-    #     if teorica.isnumeric():
-    #         infoNota["Teorica"]=teorica
-    #         bandera = False
-    #     else:
-    #         print("Dato no Valido")
-    # bandera=True
-    # while (bandera):
-    #     practica = input("Prueba practica: ")
-    #     if practica.isnumeric():
-    #         infoNota["Practica"]=practica
-    #         bandera = False
-    #     else:
-    #         print("Dato no Valido")
-    # bandera=True
-    # while (bandera):
-    #     trabajos = input("Quizes y trabajos: ")
-    #     if trabajos.isnumeric():
-    #         infoNota["Trabajos"]=trabajos
-    #         bandera = False
-    #     else:
-    #         print("Dato no Valido")
-
-    # with open("modulo/storage/notas.json") as f:
-    #     notas = json.load(f)
-
-    # notas.append(infoNota)
-    # with open("modulo/storage/notas.json", "w") as f:
-    #     data=json.dumps(notas, indent=4)
-    #     f.write(data)
-    #     f.close()
-    #     system("clear")
-    # return "Guardado Exitoso"
 
 
 def buscar():
