d22f278bdc41f1bda7dede3f4eb5dcc0400fab00
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-02-12 00:01:55 -0500

    feat:  :construction:  se configura lista ruta Netcore

--- modulo/rutas.py
+++ modulo/rutas.py
@@ -36,36 +36,66 @@ def enrutamiento():
             f.close()
             
 def lista_ruta_netcore():
+    with open("modulo/storage/trainer.json") as f:
+        trainers=json.loads(f.read())
+    with open("modulo/storage/horario1.json") as f:
+        horario1=json.loads(f.read())
+        print("Trainers:")
+        for data in horario1:
+            if data["Ruta"]=="NetCore":
+                for trainer in trainers:
+                    if trainer["Id"]==data.get('Trainer'):
+                        print(f"""
+{trainer.get('Nombre')} {trainer.get('Apellido')}
+                              """)
     with open("modulo/storage/camper.json") as f:
         campers=json.loads(f.read())
     with open("modulo/storage/horario1.json") as f:
         horario1=json.loads(f.read())
+        print("Campers:")
         for datos in horario1:
             if datos["Ruta"]=="NetCore":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"{camper.get('Nombre')} {camper.get('Apellido')}")
     with open("modulo/storage/horario2.json") as f:
         horario2=json.loads(f.read())
         for datos in horario2:
             if datos["Ruta"]=="NetCore":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"{camper.get('Nombre')} {camper.get('Apellido')}")
+                
     with open("modulo/storage/horario3.json") as f:
         horario3=json.loads(f.read())
         for datos in horario3:
             if datos["Ruta"]=="NetCore":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"{camper.get('Nombre')} {camper.get('Apellido')}")
     with open("modulo/storage/horario4.json") as f:
         horario4=json.loads(f.read())
         for datos in horario4:
             if datos["Ruta"]=="NetCore":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"{camper.get('Nombre')} {camper.get('Apellido')}")
     bandera=True
     while(bandera):
         print("\t1. Salir")
@@ -73,75 +103,133 @@ def lista_ruta_netcore():
         if opc.isnumeric():
             bandera=False#Mejorar
 
+
+
+
 def lista_ruta_nodejs():
+    with open("modulo/storage/trainer.json") as f:
+        trainers=json.loads(f.read())
     with open("modulo/storage/camper.json") as f:
         campers=json.loads(f.read())
     with open("modulo/storage/horario1.json") as f:
         horario1=json.loads(f.read())
+        print("Campers:")
         for datos in horario1:
             if datos["Ruta"]=="NodeJS":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     with open("modulo/storage/horario2.json") as f:
         horario2=json.loads(f.read())
         for datos in horario2:
             if datos["Ruta"]=="NodeJS":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     with open("modulo/storage/horario3.json") as f:
         horario3=json.loads(f.read())
         for datos in horario3:
             if datos["Ruta"]=="NodeJS":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     with open("modulo/storage/horario4.json") as f:
         horario4=json.loads(f.read())
         for datos in horario4:
             if datos["Ruta"]=="NodeJS":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     bandera=True
     while(bandera):
         print("\t1. Salir")
         opc=input()
         if opc.isnumeric():
             bandera=False#Mejorar
+
+
+
 
 def lista_ruta_java():
     with open("modulo/storage/camper.json") as f:
         campers=json.loads(f.read())
     with open("modulo/storage/horario1.json") as f:
         horario1=json.loads(f.read())
+        print("Campers:")
         for datos in horario1:
             if datos["Ruta"]=="Java":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     with open("modulo/storage/horario2.json") as f:
         horario2=json.loads(f.read())
         for datos in horario2:
             if datos["Ruta"]=="Java":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     with open("modulo/storage/horario3.json") as f:
         horario3=json.loads(f.read())
         for datos in horario3:
             if datos["Ruta"]=="Java":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     with open("modulo/storage/horario4.json") as f:
         horario4=json.loads(f.read())
         for datos in horario4:
             if datos["Ruta"]=="Java":
-                print(f"""
-                    Id:{datos.get('Campers')}
-                    """)
+                for i in range(len(datos["Campers"])):
+                    # print(f"""
+                    #     Id:{datos.get('Campers')[i]}
+                    #     """)
+                    for camper in campers:
+                        if datos.get('Campers')[i]==camper["Id"]:
+                            print(f"""
+{camper.get('Nombre')} {camper.get('Apellido')}
+                                  """)
     bandera=True
     while(bandera):
         print("\t1. Salir")
